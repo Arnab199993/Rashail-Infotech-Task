1ba1c123f911fd039967f3ef6d2e0a8981b8a4bd
Arnab199993 <103571005+Arnab199993@users.noreply.github.com>
2024-01-17 00:31:23 +0530

    update

--- vite-project/src/Components/FilterData/FilterData.jsx
+++ vite-project/src/Components/FilterData/FilterData.jsx
@@ -11,7 +11,7 @@ const FilterData = ({ handleDateChange }) => {
 
   return (
     <FormControl sx={{ width: "14rem", ml: "75%" }}>
-      <InputLabel id="simple-dropdown-label">Date Filter</InputLabel>
+      <InputLabel id="simple-dropdown-label">Filter Date</InputLabel>
       <Select
         labelId="simple-dropdown-label"
         id="simple-dropdown"
@@ -20,26 +20,26 @@ const FilterData = ({ handleDateChange }) => {
         onChange={handleChange}
       >
         <MenuItem value="">None</MenuItem>
-        <MenuItem value="1"> 1</MenuItem>
-        <MenuItem value="2"> 2</MenuItem>
-        <MenuItem value="3"> 3</MenuItem>
-        <MenuItem value="4"> 4</MenuItem>
-        <MenuItem value="5"> 5</MenuItem>
-        <MenuItem value="6"> 6</MenuItem>
-        <MenuItem value="7"> 7</MenuItem>
-        <MenuItem value="8"> 8</MenuItem>
-        <MenuItem value="9"> 9</MenuItem>
-        <MenuItem value="10"> 10</MenuItem>
-        <MenuItem value="11"> 11</MenuItem>
-        <MenuItem value="12"> 12</MenuItem>
-        <MenuItem value="13"> 13</MenuItem>
-        <MenuItem value="14"> 14</MenuItem>
-        <MenuItem value="15"> 15</MenuItem>
-        <MenuItem value="16"> 16</MenuItem>
-        <MenuItem value="17"> 17</MenuItem>
-        <MenuItem value="18"> 18</MenuItem>
-        <MenuItem value="19"> 19</MenuItem>
-        <MenuItem value="20"> 20</MenuItem>
+        <MenuItem value="1">Day 1</MenuItem>
+        <MenuItem value="2">Day 2</MenuItem>
+        <MenuItem value="3">Day 3</MenuItem>
+        <MenuItem value="4">Day 4</MenuItem>
+        <MenuItem value="5">Day 5</MenuItem>
+        <MenuItem value="6">Day 6</MenuItem>
+        <MenuItem value="7">Day 7</MenuItem>
+        <MenuItem value="8">Day 8</MenuItem>
+        <MenuItem value="9">Day 9</MenuItem>
+        <MenuItem value="10">Day 10</MenuItem>
+        <MenuItem value="11">Day 11</MenuItem>
+        <MenuItem value="12">Day 12</MenuItem>
+        <MenuItem value="13">Day 13</MenuItem>
+        <MenuItem value="14">Day 14</MenuItem>
+        <MenuItem value="15">Day 15</MenuItem>
+        <MenuItem value="16">Day 16</MenuItem>
+        <MenuItem value="17">Day 17</MenuItem>
+        <MenuItem value="18">Day 18</MenuItem>
+        <MenuItem value="19">Day 19</MenuItem>
+        <MenuItem value="20">Day 20</MenuItem>
       </Select>
     </FormControl>
   );
